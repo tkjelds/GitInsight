8362f5b766efff2f85d0d239bacd5a5b3d1222b7
tkjelds <tkjelds385@gmail.com>
2022-10-25 14:40:52 +0200

    added quick usings.cs and some quick code memes

--- GitInsight.app/Program.cs
+++ GitInsight.app/Program.cs
@@ -4,7 +4,11 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world");
+            // var _repo = args[0]; // Path til repo
+            // var _mode = args[1]; //Frequence = 0 | Author = 1
+            // Repository repo = new Repository(_repo); // lav repo object
+            // repo.Commits.GroupBy( u => u.Author); // sorter på Author
         }
     }  
 }
